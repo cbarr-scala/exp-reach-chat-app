bbad4ae2697de0b292581b45377822966b429023
Chris Barr <chris.barr@scala.com>
2017-08-22 13:55:17 -0400

    initial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'testing .. 1. 2. 3', 'halp, gimmeh the image asdf!'], ' ');
+document.addEventListener('DOMContentLoaded', () => {
 
-  return element;
-}
+  document
+    .getElementById('send-it-button')
+    .addEventListener('click', () => {
+      exp.getCurrentDevice().then((device) => {
+        const location = device.document.location;
 
-document.body.appendChild(component());
+        let locationName = location.name || 'Anon';
+
+        // chat app if having a zone, should only be in one zone, so we hard code to this
+        if (location.zones && location.zones.length > 0) {
+          locationName += ` - ${location.zones[0].name}`;
+        }
+
+        exp
+          .getChannel('testChannel')
+          .broadcast(
+            'messageSent',
+            {
+              text: document.getElementById('text-to-send-textbox').value,
+              submitter: locationName,
+            },
+            2000,
+          );
+      });
+    });
+});
+
+// eslint-disable-next-line no-unused-vars
+window.play = function play() {
+  exp.getChannel('testChannel').listen('messageSent', (payload, respondCallback) => {
+
+    const element = document.getElementById('what-was-sent-container');
+
+    element.innerHTML = `${element.innerHTML}<p>${payload.submitter} says: ${payload.text}</p>`;
+
+    respondCallback();
+  });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    app: './src/index.js',
   },
   devtool: 'inline-source-map',
-  devServer: { 
-    contentBase: './dist'
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.ejs'
+      template: 'src/index.ejs',
     }),
     new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'src/exp', to: '' },
+    ]),
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
